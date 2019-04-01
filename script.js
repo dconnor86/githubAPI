c7c7c5901ab0cda87739f0e924e5bfa9fbda6650
dconnor86 <dconnor86@gmail.com>
2019-04-01 19:27:33 -0400

    working buttons, console shows API array

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let URL = 'https://api.github.com/users/dconnor86/repos'
-
-fetch ('https://api.github.com/users/dconnor86/repos')
+function searchGithub() {
+let userName = document.getElementById('gitSearchId').value;
+let URL = 'https://api.github.com/users/'+userName+'/repos'
+let maxResults = $('#js-max-results').val();
+fetch (URL)
 .then(response => response.json())
 .then(responseJson => console.log(responseJson))
 .catch(error => alert('ERROR!'));
-    
-    
-    
+}
+
+function renderSubmit() {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        searchGithub();
+        
+    })
+}
+
+
+
+
+renderSubmit();
