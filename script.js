061627f4f8ab316773e53e0a16ea382e96050c7d
dconnor86 <46802000+dconnor86@users.noreply.github.com>
2019-04-02 21:12:17 -0400

    Finished working project

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function searchGithub() {
 let userName = document.getElementById('gitSearchId').value;
-let URL = 'https://api.github.com/users/'+userName+'/repos'
-let maxResults = $('#js-max-results').val();
-fetch (URL)
-.then(response => response.json())
-.then(responseJson => console.log(responseJson))
-.catch(error => alert('ERROR!'));
+let URL = `https://api.github.com/users/${userName}/repos`
+    fetch (URL)
+    .then(response => response.json())
+    .then(responseJson => results(responseJson))
+    .catch(error => alert('ERROR!'));
+}
+
+function results(data, maxResults){
+    for(let i = 0; i < data.length & i < ($('#js-max-results').val()); i++){
+    data[i].name;
+    data[i].html_url; 
+    $('.reposForUser').append(
+        `<a href= ${data[i].html_url}>${data[i].name} </a><br>`
+    );
+    }
 }
 
 function renderSubmit() {
     $('form').on('submit', function(event) {
         event.preventDefault();
-        searchGithub();
-        
-    })
+        $('.reposForUser').empty();
+        searchGithub()
+        })
 }
-
-
-
 
 renderSubmit();
